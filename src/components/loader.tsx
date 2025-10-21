853c881de3a8e7fa745879e8553bfa6d812e3638
pablo-aviles-prieto <pablo.aviles.prieto@gmail.com>
2025-10-21 23:12:35 +0200

    feat(tests): add e2e

--- src/components/loader.tsx
+++ src/components/loader.tsx
@@ -1,7 +1,7 @@
 import { Loader2 } from 'lucide-react';
 
 export const Loader = () => (
-	<div className='flex min-h-24 items-center justify-center bg-inherit'>
+	<div className='flex min-h-24 items-center justify-center bg-inherit' data-testid='loader'>
 		<Loader2 className='size-8 animate-spin text-primary' />
 	</div>
 );
